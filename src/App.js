d6280b371487a1997a88ea13f2a79e43e7265dc1
yashkr90 <kryash90@gmail.com>
2023-02-09 07:22:17 +0530

    create froms and used onChange to change the details of input as user types

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [detail, setDetail] = useState({
+    fname: " qwert",
+    lname: "yuio",
+    email: "yh@kk.vom",
+    phone: 4545,
+  });
+
+  const HandleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    console.log(name, value);
+
+    // if(name=='fname')
+    // {
+    //   setDetail({fname:value,
+    //     lname:"",
+    //     email:"",
+    //     phone:""});
+    // }
+    setDetail((prevdata) => {
+      return {
+        ...prevdata,
+        [name]: value,
+      };
+    });
+  };
+  const Placeitems = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <div className="maindiv">
+          <h1>
+            Enter your details
+            {detail.fname} {detail.lname}
+          </h1>
+          <div className="formdiv">
+            <input
+              type="text"
+              name="fname"
+              placeholder="Enter first name"
+              // value={fname}
+              onChange={HandleChange}
+            ></input>
+            <input
+              type="text"
+              name="lname"
+              placeholder="Enter last name"
+              // value={lname}
+
+              onChange={HandleChange}
+            ></input>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={HandleChange}
+            ></input>
+            <input
+              type="number"
+              name="number"
+              placeholder="Enter phone no"
+              onChange={HandleChange}
+            ></input>
+            <button onClick={Placeitems}> Submit</button>
+          </div>
+        </div>
+      </form>
+    </>
   );
 }
 
